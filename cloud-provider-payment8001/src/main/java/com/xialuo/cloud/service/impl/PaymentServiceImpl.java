4959478345ee34e9cf563dcc0096469ae712d5b4
冯立业 <fengliye@xdf.cn>
2020-12-19 14:20:57 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/xialuo/cloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,30 @@
+package com.xialuo.cloud.service.impl;
+
+import com.xialuo.cloud.entity.Payment;
+import com.xialuo.cloud.dao.PaymentDao;
+import com.xialuo.cloud.service.PaymentService;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 10:40
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
