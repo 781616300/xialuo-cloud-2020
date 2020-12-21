b32befdc01b375d3b551ded1fc082f631be53d96
冯立业 <fengliye@xdf.cn>
2020-12-21 16:19:38 +0800

    ribbon

--- cloud-consumer-order80/src/main/java/com/xialuo/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/xialuo/cloud/controller/OrderController.java
@@ -2,8 +2,13 @@ package com.xialuo.cloud.controller;
 
 import com.xialuo.cloud.entity.CommonResult;
 import com.xialuo.cloud.entity.Payment;
+import com.xialuo.cloud.lb.LoadBalancer;
+import java.net.URI;
+import java.util.List;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +26,10 @@ public class OrderController
     @Resource
     private RestTemplate restTemplate;
 
-//    @Resource
-//    private LoadBalancer loadBalancer;
-//    @Resource
-//    private DiscoveryClient discoveryClient;
+    @Resource
+    private LoadBalancer loadBalancer;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment)
@@ -50,22 +55,26 @@ public class OrderController
         }
     }
 
-//    @GetMapping(value = "/consumer/payment/lb")
-//    public String getPaymentLB()
-//    {
-//        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-//
-//        if(instances == null || instances.size() <= 0)
-//        {
-//            return null;
-//        }
-//
-//        ServiceInstance serviceInstance = loadBalancer.instances(instances);
-//        URI uri = serviceInstance.getUri();
-//
-//        return restTemplate.getForObject(uri+"/payment/lb",String.class);
-//
-//    }
+    /**
+     * 描述: 手写简单版本 ribbon负载均衡算法
+     * 测试时一定要注释掉 com.xialuo.cloud.config.ApplicationContextConfig 重的@LoadBalanced.
+     **/
+    @GetMapping(value = "/consumer/payment/lb")
+    public String getPaymentLB()
+    {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        if(instances == null || instances.size() <= 0)
+        {
+            return null;
+        }
+
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
+
+    }
 //
 //    // ====================> zipkin+sleuth
 //    @GetMapping("/consumer/payment/zipkin")
