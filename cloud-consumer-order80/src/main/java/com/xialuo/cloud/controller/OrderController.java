eb37a0e9652f02c5f8462b42e70e0dcc6f7e4529
冯立业 <fengliye@xdf.cn>
2020-12-27 22:06:20 +0800

    SpringCloud Sleuth分布式链路跟踪

--- cloud-consumer-order80/src/main/java/com/xialuo/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/xialuo/cloud/controller/OrderController.java
@@ -75,12 +75,12 @@ public class OrderController
         return restTemplate.getForObject(uri+"/payment/lb",String.class);
 
     }
-//
-//    // ====================> zipkin+sleuth
-//    @GetMapping("/consumer/payment/zipkin")
-//    public String paymentZipkin()
-//    {
-//        String result = restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/", String.class);
-//        return result;
-//    }
+
+    // ====================> zipkin+sleuth
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin()
+    {
+        String result = restTemplate.getForObject("http://127.0.0.1:8001"+"/payment/zipkin/", String.class);
+        return result;
+    }
 }
